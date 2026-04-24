fef3a324459ff6fe318f218a62d828605696959f
Robbe De Busser <debusser.r@hotmail.com>
2026-04-24 15:45:57 +0200

    fixed some navbar bugs

--- src/main.js
+++ src/main.js
@@ -105,16 +105,18 @@ window.addEventListener("load", () => {
     const onScroll = () => {
       if (window.scrollY > 20) {
         mainHeader.classList.add("shadow-xl");
-        mainHeader.style.height = "70px";
-        headerInner.style.paddingTop = "0.5rem";
-        headerInner.style.paddingBottom = "0.5rem";
-        headerLogo.style.height = "32px";
-        mainContent.style.paddingTop = "70px";
+        mainHeader.style.height = "60px";
+        headerInner.style.paddingTop = "0";
+        headerInner.style.paddingBottom = "0";
+        headerInner.style.height = "60px";
+        headerLogo.style.height = "28px";
+        mainContent.style.paddingTop = "60px";
       } else {
         mainHeader.classList.remove("shadow-xl");
         mainHeader.style.height = "80px";
         headerInner.style.paddingTop = "1rem";
         headerInner.style.paddingBottom = "1rem";
+        headerInner.style.height = "80px";
         headerLogo.style.height = "40px";
         mainContent.style.paddingTop = "80px";
       }
